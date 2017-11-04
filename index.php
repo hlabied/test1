b103c167e07ce3470aade533e3ca9e59a0b34d81
hlabied <hlabied@gmail.com>
2017-11-04 15:56:52 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Test</title>
+  </head>
+  <body>
+
+
+<P>
+
+  لوريم اوبسوم شسشسشسشس المغرب  سيسيسي
+ المسار الجوي للارض   <br>
+<strong> المجال الكهرمغناطيسي</strong><br>
+  دول العالم الثالث
+
+
+</P>
+
+
+  </body>
+</html>
